180f8097f8f821609274164a581b8d12e110e040
S. Roland Marshall <samuel.r.marshall@gmail.com>
2023-03-14 23:52:04 -0400

    timer counts down

--- client/src/components/AgendaPreview/AgendaPreview.jsx
+++ client/src/components/AgendaPreview/AgendaPreview.jsx
@@ -4,6 +4,7 @@ import ReactMarkdown from "react-markdown";
 const AgendaPreview = ({ agenda, meetingTime, timerRunning }) => {
   const [agendaWithTime, setAgendaWithTime] = useState("");
   const [h2Timers, setH2Timers] = useState({});
+  const [currentH2Index, setCurrentH2Index] = useState(0);
 
   useEffect(() => {
     if (agenda.trim() === "") {
@@ -12,27 +13,46 @@ const AgendaPreview = ({ agenda, meetingTime, timerRunning }) => {
     }
 
     const splitAgenda = agenda.split(/[\n\r]/);
-    const numH2s = splitAgenda.filter((line) => line.startsWith("##")).length;
-    const h2TimeSeconds = Math.round((meetingTime * 60) / numH2s);
-
-    let agendaWithDuration = "";
-    let h2TimerValues = {};
-    let h2Index = 1;
-
-    splitAgenda.forEach((line) => {
-      if (line.startsWith("##")) {
-        const h2Timer = convertSecondsToTime(h2TimeSeconds);
-        agendaWithDuration += `${line} (${h2Timer})\n`;
-        h2TimerValues[h2Index] = h2TimeSeconds;
-        h2Index++;
-      } else {
-        agendaWithDuration += `${line}\n`;
-      }
-    });
+    const agendaWithDuration = splitAgenda
+      .map((line, index) => {
+        if (line.startsWith("##")) {
+          const h2Timer = convertSecondsToTime(h2Timers[index]);
+          return `${line} (${h2Timer})`;
+        }
+        return line;
+      })
+      .join("\n");
 
     setAgendaWithTime(agendaWithDuration);
-    setH2Timers(h2TimerValues);
-  }, [agenda, meetingTime]);
+  }, [agenda, h2Timers]);
+
+  useEffect(() => {
+    if (!timerRunning) {
+      return;
+    }
+
+    const timer = setInterval(() => {
+      setH2Timers((timers) => {
+        // Find the next non-zero timer, starting from the current index
+        let index = currentH2Index;
+        while (timers[index] === 0 && index <= Object.keys(timers).length) {
+          index++;
+        }
+        if (index > Object.keys(timers).length) {
+          // All timers have reached 0, stop the interval
+          clearInterval(timer);
+          return timers;
+        }
+        const updatedTimer = { ...timers };
+        updatedTimer[index] = timers[index] - 1;
+        setCurrentH2Index(index);
+        return updatedTimer;
+      });
+    }, 1000);
+
+    // Clear the interval when the component unmounts or the timer is stopped
+    return () => clearInterval(timer);
+  }, [timerRunning, currentH2Index]);
 
   // Function to convert seconds to time notation (MM:SS)
   const convertSecondsToTime = (time) => {
